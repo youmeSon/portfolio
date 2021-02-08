f2103b0ae9de00401eb336ef13a867b9b7517327
Youme <winellyoume@gmail.com>
2021-02-08 15:14:45 +0000

    JS_Navbar

---
+++ main.js
@@ -0,0 +1,15 @@
+'use strict'
+
+// Make navbar transparent it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener('scroll', () => {
+  console.log(window.scrollY);
+  console.log(navbarHeight);
+  if(window.scrollY > navbarHeight) {
+    navbar.classList.add('navbar--dark')
+  } else {
+    navbar.classList.remove('navbar--dark')
+
+  }
+} )
