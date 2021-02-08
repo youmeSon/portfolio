b1cdd3af6b2aefe7ddede54b8a6da58b136d0785
Youme <winellyoume@gmail.com>
2021-02-08 18:10:18 +0000

    JS_Home contact button

--- main.js
+++ main.js
@@ -19,8 +19,17 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null) {
     return;
   } 
-
-  console.log(event.target.dataset.link);
-  const scrollTo = document.querySelector(link);
-  scrollTo.scrollIntoView({behavior: "smooth"});
+  scrollIntoView(link);
 })
+
+//Handle click on "contact me" button on home 
+const contactMe = document.querySelector('.home__contact');
+contactMe.addEventListener('click', () => {
+  scrollIntoView('#contact');
+})
+
+// Methods
+function scrollIntoView(selector) {
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({behavior: "smooth"});
+}
