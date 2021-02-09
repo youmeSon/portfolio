3371647678a6541c73928297efb11dc99e31097d
Youme <winellyoume@gmail.com>
2021-02-09 15:23:32 +0000

    JS_Arrow up

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 function scrollIntoView(selector) { //우리가 정의한 함수
   const scrollTo = document.querySelector(selector);
   scrollTo.scrollIntoView({behavior: "smooth"}); // DOM 요소의 함수
-}
+};
 
 // Make navbar transparent it is on the top
 const navbar = document.querySelector('#navbar');
@@ -15,7 +15,7 @@ document.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('navbar--dark')
   }
-})
+});
 
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
@@ -26,13 +26,13 @@ navbarMenu.addEventListener('click', (event) => {
     return;
   } 
   scrollIntoView(link);
-})
+});
 
 //Handle click on "contact me" button on home 
 const contactMe = document.querySelector('.home__contact');
 contactMe.addEventListener('click', () => {
   scrollIntoView('#contact');
-})
+});
 
 //Make home slowly fade to trasparent as the window scrolls down
 const home = document.querySelector('#home');
@@ -41,14 +41,30 @@ const homeContainer = document.querySelector('.home__container')
 document.addEventListener('scroll', () => {
   homeContainer.style.opacity = 1 - window.scrollY / homeHeight;
   contactMe.style.opacity = 1 - window.scrollY / homeHeight;
-})
+});
 
 // When mouse on 'contact me' button -> recover opacity
 contactMe.addEventListener('mouseenter', () => {
   contactMe.style.opacity = 1;
-})
+});
 
 // When mouse off 'contact me' button -> lose opacity again 
 contactMe.addEventListener('mouseleave', () => {
   contactMe.style.opacity = 1 - window.scrollY / homeHeight;
-})
+});
+
+// Show 'Arrow u' button when scrolling down
+const arrowBtn = document.querySelector('.arrow-btn');
+
+document.addEventListener('scroll', () => {
+  if(window.scrollY > homeHeight / 2) {
+    arrowBtn.classList.add('visible');
+  } else {
+    arrowBtn.classList.remove('visible');
+  }
+});
+
+// Handle click on the 'arrow up' button
+arrowBtn.addEventListener('click', () => {
+  scrollIntoView('#home');
+});
