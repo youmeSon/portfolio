f8d9157382ac1cccdcfab9bea6935104ed7b30ca
Youme <winellyoume@gmail.com>
2021-02-10 14:57:26 +0000

    JS_Project filtering

--- main.js
+++ main.js
@@ -68,3 +68,28 @@ document.addEventListener('scroll', () => {
 arrowBtn.addEventListener('click', () => {
   scrollIntoView('#home');
 });
+
+// Projects
+const workBtnContainer = document.querySelector('.work__categories');
+const projectContainer = document.querySelector('.work__projects');
+const projects = document.querySelectorAll('.project');
+workBtnContainer.addEventListener('click', (e) => {
+  const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
+  if( filter == null) {
+    return;
+  }
+  projectContainer.classList.add('anim-out');
+
+  setTimeout(() => {
+    projects.forEach((project) => {
+      if( filter === '*' || filter === project.dataset.type) {
+        project.classList.remove('invisible')
+      } else {
+        project.classList.add('invisible')
+      }
+    });
+    projectContainer.classList.remove('anim-out');
+  }, 300);
+
+})
+
