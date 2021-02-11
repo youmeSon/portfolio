5a2fe54153834811887b4df0526282ad1529050c
Youme <winellyoume@gmail.com>
2021-02-11 09:13:18 +0000

    JS_Remove active from nav

--- main.js
+++ main.js
@@ -78,8 +78,16 @@ workBtnContainer.addEventListener('click', (e) => {
   if( filter == null) {
     return;
   }
-  projectContainer.classList.add('anim-out');
 
+  // Remove selection from the previous item and select the new one
+  const active = document.querySelector('.category__btn.selected');
+  active.classList.remove('selected');
+  const target = 
+    e.target.nodeName === 'BUTTON' ? e.target : 
+    e.target.parentNode;
+  target.classList.add('selected');
+
+  projectContainer.classList.add('anim-out');
   setTimeout(() => {
     projects.forEach((project) => {
       if( filter === '*' || filter === project.dataset.type) {
@@ -91,5 +99,5 @@ workBtnContainer.addEventListener('click', (e) => {
     projectContainer.classList.remove('anim-out');
   }, 300);
 
-})
+});
 
